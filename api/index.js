80dcc139c35d4024e9ec3071060505ac90551eb5
wassimelhamdaoui <wassim.elhamdaoui@uit.ac.ma>
2024-02-13 07:02:26 +0100

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import { dot } from 'dotenv';
+
+
+dotenv.config();
+mongoose.connect(process.env.MONGO
+).then(() => {
+    console.log('Connected to MongoDB')
+}).catch((err) => {
+    console.log('Failed to connect to MongoDB', err)
+});
+
 
 const app = express();
 
