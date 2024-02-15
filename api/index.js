f703f92c34bd19583f2c511497c353a1c87f601a
wassimelhamdaoui <wassim.elhamdaoui@uit.ac.ma>
2024-02-15 08:45:58 +0100

    create a test api route

--- api/index.js
+++ api/index.js
@@ -2,18 +2,20 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { dot } from 'dotenv';
 
-
 dotenv.config();
-mongoose.connect(process.env.MONGO
-).then(() => {
-    console.log('Connected to MongoDB')
-}).catch((err) => {
-    console.log('Failed to connect to MongoDB', err)
-});
 
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log('Failed to connect to MongoDB', err);
+});
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
-    });
+});
+
+app.get('/', (req, res) => {
+    res.json({message: "welcome to the Api!"})
+});
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    userbame: {
+    username: {
         type: String,
         required: true,
         unique: true,
