cbc04d4ebfc6a6783cc556b6e4f8bbb188ff4f67
wassimelhamdaoui <wassim.elhamdaoui@uit.ac.ma>
2024-02-16 06:24:29 +0100

    add the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,19 +7,22 @@ import Dashboard from './pages/Dashboard'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/signup' element={<SignUp />} />
-      </Routes>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+    
+        </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -8,6 +8,7 @@ export default function signUp() {
   const [errorMessages, setErrorMessages] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+
   const handleChange = (e) => {
     setFormData({...formData, [e.target.id]: e.target.value.trim()});
   }
